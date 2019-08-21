957e8c2c8ccbcf07c8c794e1e6877c8c87ac24d2
morinoko <forby.1@gmail.com>
2019-08-21 13:54:26 +0900

    add tests for products index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select "h1", "Products"
+    assert_select "tbody tr", 3
+    assert_select ".description h1", 'Programming Ruby 1.9'
   end
 
   test "should get new" do
