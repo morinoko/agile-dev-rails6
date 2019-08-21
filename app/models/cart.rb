92c24e3d7bf7231ec7a231a2809f19af8cb11320
morinoko <forby.1@gmail.com>
2019-08-21 16:11:03 +0900

    add cart and related tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  # If we destroy a cart, we want Rails also to
+  # destroy any line items that are associated with that cart.
 end
