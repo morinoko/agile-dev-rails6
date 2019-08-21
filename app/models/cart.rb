cce02d8907b2454d781c217dda0377c980e5268d
morinoko <forby.1@gmail.com>
2019-08-22 08:23:48 +0900

    show line items grouped by quatity in cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,16 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   # If we destroy a cart, we want Rails also to
   # destroy any line items that are associated with that cart.
+
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+
+    current_item
+  end
 end
