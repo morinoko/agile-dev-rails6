d10f6deb92b9b4efb37c9eb0e1ccf3d73e250b25
morinoko <forby.1@gmail.com>
2019-08-23 18:04:35 +0900

    make sure lib folder is in the path

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Depot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.eager_load_paths << "#{Rails.root}/lib/path"
 
     config.filter_parameters += [ :credit_card_number ]
 
