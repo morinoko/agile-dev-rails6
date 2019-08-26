6549b4b5609b563a24fa20c43462842a973ce0f5
morinoko <forby.1@gmail.com>
2019-08-26 14:29:27 +0900

    add a locale switcher

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/locale_switcher")
 
 // I18n setup
 window.I18n = require("../../../public/javascripts/i18n")
