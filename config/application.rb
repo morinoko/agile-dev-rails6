d2152af49bf0a4d049bb65d018ef0e2b95c192b7
morinoko <forby.1@gmail.com>
2019-08-26 13:58:04 +0900

    set up i18n for javascript

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Depot
     config.filter_parameters += [ :credit_card_number ]
 
     config.require_master_key = true
+
+    config.middleware.use I18n::JS::Middleware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
